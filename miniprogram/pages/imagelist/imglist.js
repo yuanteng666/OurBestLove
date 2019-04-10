f01d36831111bd4584d20d444403cff3b54ac691
yuanteng666 <2890236675@qq.com>
2019-04-10 10:41:02 +0800

    'newbranch'

--- miniprogram/pages/imagelist/imglist.js
+++ miniprogram/pages/imagelist/imglist.js
@@ -14,38 +14,10 @@ Page({
         tip: '自营'
       },
       {
-        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product2.jpg',
-        title: 'Apple iPad 平板电脑 2018年新款9.7英寸',
-        originalPrice: 3499,
-        favourPrice: 3399,
-        tip: '优惠'
-      },
-      {
-        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product3.jpg',
-        title: 'Apple MacBook Pro 13.3英寸笔记本电脑（2017款Core i5处理器/8GB内存/256GB硬盘 MupxT2CH/A）',
-        originalPrice: 12999,
-        favourPrice: 10688,
-        tip: '秒杀'
-      },
-      {
-        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product4.jpg',
-        title: 'Kindle Paperwhite电纸书阅读器 电子书墨水屏 6英寸wifi 黑色',
-        originalPrice: 999,
-        favourPrice: 958,
-        tip: '秒杀'
-      },
-      {
-        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product5.jpg',
-        title: '微软（Microsoft）新Surface Pro 二合一平板电脑笔记本 12.3英寸（i5 8G内存 256G存储）',
-        originalPrice: 8888,
-        favourPrice: 8288,
-        tip: '优惠'
-      },
-      {
-        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product6.jpg',
-        title: 'Apple Watch Series 3智能手表（GPS款 42毫米 深空灰色铝金属表壳 黑色运动型表带 MQL12CH/A）',
-        originalPrice: 2899,
-        favourPrice: 2799,
+        image: 'https://img-cdn-qiniu.dcloud.net.cn/uploads/example/product1.jpg',
+        title: 'Apple iPhone X 256GB 深空灰色 移动联通电信4G手机',
+        originalPrice: 9999,
+        favourPrice: 8888,
         tip: '自营'
       }
     ]
