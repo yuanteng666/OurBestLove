4792a9f6a840aeeffa1cd56a65fa7ad46e44f696
yuanteng666 <2890236675@qq.com>
2019-04-19 17:24:10 +0800

    '做完天气界面'

--- miniprogram/pages/weather/weather.js
+++ miniprogram/pages/weather/weather.js
@@ -11,20 +11,39 @@ Page({
     weatherDatas:[],
     amapPlugin: null,
     key: 'c80a0724125ba2b893265e10a54863b2',
-    cityId:''
+    //默认内乡县
+    cityId:'101180706',
+    postiondes:'',
+    cityArr:[{
+      name:'内乡县',
+      id:'101180706'
+    },
+      {
+        name: '南阳市',
+        id: '101180701'
+      },
+      {
+        name: '朝阳区',
+        id: '101010300'
+      },
+      {
+        name: '郑州市',
+        id: '101180101'
+      }],
+      index:0
   },
  
   onLoad: function (e) {
-    // this.data.amapPlugin = new amap.AMapWX({
-    //   key: this.data.key
-    // });
-    // this.getRegeo();
+    this.data.amapPlugin = new amap.AMapWX({
+      key: this.data.key
+    });
+    this.getRegeo();
     this.getWeather()
   },
   getWeather(){
     const _this = this;
     wx.request({
-      url: 'https://www.tianqiapi.com/api/?version=v1&cityid=101180706',
+      url: 'https://www.tianqiapi.com/api/?version=v1&cityid=' + this.data.cityId,
       method:'GET',
       success(res){
         let arr = [];
@@ -36,8 +55,13 @@ Page({
         _this.setData({
           weatherDatas : arr
         });
-        _this.createWxChart();
-        _this.createLineChart2();
+        if(lineChart == null){
+          _this.createWxChart();
+          _this.createLineChart2();
+        }else{
+          _this.updateData();
+          _this.updateData2();
+        }
       },
       fail(err){
 
@@ -177,15 +201,71 @@ Page({
       success: (data) => {
         console.log(JSON.stringify(data))
         
-        // this.data.addressName = data[0].name;
-        // _this.setData({
-        //   localPosition: data[0].desc
-        // })
+        _this.setData({
+          postiondes: data[0].name
+        })
       },
       fail: (data) => {
         console.log(JSON.stringify(data))
-
       }
+    });
+  },
+  bindPickerChange(e){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    const _this = this;
+    this.setData({
+      index: e.detail.value,
+      cityId: _this.data.cityArr[e.detail.value].id
+    })
+    this.getWeather()
+  },
+  updateData(){
+    let hours = this.data.weatherDatas[0].hours;
+    let categories_chart1 = [];
+    let wenduData = [];
+    for (let item of hours) {
+      categories_chart1.push(item.day.substring(item.day.indexOf('日') + 1))
+      wenduData.push(item.temNum)
+    }
+    let series =  [{
+      name: '今日天气趋势',
+      data: wenduData,
+      format: function (val, name) {
+        return val + '℃';
+      }
+    }];
+    lineChart.updateData({
+      categories: categories_chart1,
+      series: series
+    });
+  },
+  updateData2(){
+    let categories = [];
+    let wenduDataMin = [];
+    let wenduDataMax = [];
+    for (let item of this.data.weatherDatas) {
+      categories.push(item.day)
+      wenduDataMin.push(item.temNum2)
+      wenduDataMax.push(item.temNum1)
+    }
+
+   let  series =  [{
+      name: '最低温度',
+      data: wenduDataMin,
+      format: function (val, name) {
+        return val + '℃';
+      }
+    },
+    {
+      name: '最高温度',
+      data: wenduDataMax,
+      format: function (val, name) {
+        return val + '℃';
+      }
+    }]
+    lineChart2.updateData({
+      categories: categories,
+      series: series
     });
   }
 });
